a2c1a20dabaa6580607037798e07842ee8a6b3e9
Brandon Omar Ruíz Becerril <brandon.ruiz@salud.gob.mx>
2020-03-20 14:47:25 -0600

    titulo

--- admin/sistema/reporteoncologico.php
+++ admin/sistema/reporteoncologico.php
@@ -34,7 +34,7 @@ $proceso = $_POST['proceso'];
           <div class="page-header row no-gutters py-4">
             <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
               <span class="text-uppercase page-subtitle">Reporte</span>
-              <h3 class="page-title">General de Medicamentos</h3>
+              <h3 class="page-title">General de Medicamentos Oncológicos</h3>
             </div>
           </div>
             <!-- End Page Header -->
