d69da932939499bba7e952fb5dabf9ddfac62302
Brandon Omar Ruíz Becerril <brandon.ruiz@salud.gob.mx>
2020-03-20 20:16:03 -0600

    todo

--- admin/sistema/home.php
+++ admin/sistema/home.php
@@ -3,7 +3,14 @@
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>     <center>Licitación 2020.
+    <font face="Comic Sans MS,arial,verdana">  <h1 style="font-size:50px; background-color: background: rgba(169,3,41,1);
+background: -moz-linear-gradient(left, rgba(169,3,41,1) 0%, rgba(143,2,34,0.88) 44%, rgba(109,0,25,0.72) 100%);
+background: -webkit-gradient(left top, right top, color-stop(0%, rgba(169,3,41,1)), color-stop(44%, rgba(143,2,34,0.88)), color-stop(100%, rgba(109,0,25,0.72)));
+background: -webkit-linear-gradient(left, rgba(169,3,41,1) 0%, rgba(143,2,34,0.88) 44%, rgba(109,0,25,0.72) 100%);
+background: -o-linear-gradient(left, rgba(169,3,41,1) 0%, rgba(143,2,34,0.88) 44%, rgba(109,0,25,0.72) 100%);
+background: -ms-linear-gradient(left, rgba(169,3,41,1) 0%, rgba(143,2,34,0.88) 44%, rgba(109,0,25,0.72) 100%);
+background: linear-gradient(to right, rgba(169,3,41,1) 0%, rgba(143,2,34,0.88) 44%, rgba(109,0,25,0.72) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a90329', endColorstr='#6d0019', GradientType=1 );;color:white;" >     <center >Licitación 2020</center></font>
 
         </h1>
         <center>
